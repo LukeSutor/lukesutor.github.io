c41ff873a9864d51187b4e847af86438d74191ea
Luke Sutor <lukesutor@gmail.com>
2024-08-26 22:14:43 -0400

    light styling for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,15 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="my-16 font-semibold text-3xl lg:text-7xl">404: Not Found</h1>
+      <p>I don't think you meant to come here...</p>
+      <Link to="/" className="font-semibold text-lg px-4 py-2 mt-4 mx-auto bg-blue-200 border border-blue-300 rounded-lg shadow hover:shadow-md">Go home</Link>
+    </div>
   </Layout>
 )
 
