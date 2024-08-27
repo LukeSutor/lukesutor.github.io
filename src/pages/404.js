0d5fbd5bccd52fa8b6829840632e6a45586ee966
Luke Sutor <lukesutor@gmail.com>
2024-08-26 22:20:00 -0400

    format and remove bloat

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,9 +7,16 @@ import { Link } from "gatsby"
 const NotFoundPage = () => (
   <Layout>
     <div className="flex flex-col justify-center items-center">
-      <h1 className="my-16 font-semibold text-3xl lg:text-7xl">404: Not Found</h1>
+      <h1 className="my-16 font-semibold text-3xl lg:text-7xl">
+        404: Not Found
+      </h1>
       <p>I don't think you meant to come here...</p>
-      <Link to="/" className="font-semibold text-lg px-4 py-2 mt-4 mx-auto bg-blue-200 border border-blue-300 rounded-lg shadow hover:shadow-md">Go home</Link>
+      <Link
+        to="/"
+        className="font-semibold text-lg px-4 py-2 mt-4 mx-auto bg-blue-200 border border-blue-300 rounded-lg shadow hover:shadow-md"
+      >
+        Go home
+      </Link>
     </div>
   </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import "../styles/global.css"
 
 import Layout from "../components/layout"
 import Hero from "../components/hero"
